eaaca52ddacf027cb922ce65262916893cc93364
Kunal Nepali <kunalnepali123456@gmail.com>
2025-12-30 19:12:34 +0545

    In Home Page, Price is shown in NRs. In all other pages, price is in $, that needs to be changes ASAP.

--- obj/Debug/net8.0/ComBag.AssemblyInfo.cs
+++ obj/Debug/net8.0/ComBag.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComBag")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03903f876c12048bb784cc7fa3800c865dba2bbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aebe40e07b0dda9640e9ebce192ab7838f45ae3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComBag")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComBag")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
